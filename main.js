f520af5bb91e5b9dad748ba5deb3084a9eeda17e
yaskyj <yaskyj@yahoo.com>
2015-01-29 18:35:13 -0600

    Video loading

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-var tag = document.createElement('script');
+var urlValue,
+  re = /=(\w+)/,
+  videoID = '',
+  tag,
+  firstScriptTag,
+  player,
+  done = false;
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
     height: '390',
     width: '640',
-    videoId: 'M7lc1UVf-VE',
+    videoId: videoID,
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
@@ -21,7 +22,6 @@ function onPlayerReady(event) {
   event.target.playVideo();
 }
 
-var done = false;
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING && !done) {
     setTimeout(stopVideo, 6000);
@@ -35,16 +35,23 @@ function stopVideo() {
 
 $(document).ready(function() {
   $('#the-button').click(function() {
-    var urlValue = $('#link-value').val(),
-    re = /=(\w+)$/,
+    urlValue = $('#link-value').val();
+    re = /=(\w+)/;
     videoID;
+    tag = document.createElement('script');
+    firstScriptTag = document.getElementsByTagName('script')[0];
+    
     if (urlValue.length > 11) {
       videoID = urlValue.match(re)[1].trim(); 
     }
     else {
       videoID = urlValue.trim();
     }
+
     $('.for-display').append('This is the video ID ' + videoID);
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
   });
 });

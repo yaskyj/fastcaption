0bd904ed4a7d22763928cc8eaa8f0f4ed21f9cce
yaskyj <yaskyj@yahoo.com>
2015-02-12 20:14:13 -0600

    Figured out asr url

--- request-test.js
+++ request-test.js
@@ -3,16 +3,22 @@ var request = require('request'),
     tempArr,
     data;
 
-request('https://www.youtube.com/watch?v=fPloDzu_wcI', function (error, response, body) {
+request('https://www.youtube.com/api/timedtext?sparams=asr_langs,caps,v,expire&expire=1423807305&asr_langs=en,pt,de,it,ru,ko,es,ja,nl,fr&hl=en_US&signature=E24D36B66C0F37BA26B699E50B3EC7BA41411260.7417BF773A8210565ACFC5F2D8321F4916B657E4&v=fPloDzu_wcI&key=yttt1&caps=asr&type=track&lang=en&name&kind=asr&fmt=1', function (error, response, body) {
+  if (error) {
+    console.log(response);
+  }
   if (!error && response.statusCode == 200) {
-    data = body.split('&');
-    for (var i in data) {
-      tempArr = data[i].split('=');
-      dataObj[tempArr[0]] = tempArr[1];
-      console.log(i);
-      console.log(tempArr[0] + ' : ' + tempArr[1]);
-    }
+    // console.log(response);
+    console.log(body);
+    // data = body.split('&');
+    // for (var i in data) {
+    //   tempArr = data[i].split('=');
+    //   dataObj[tempArr[0]] = tempArr[1];
+    //   console.log(i);
+    //   console.log(tempArr[0] + ' : ' + tempArr[1]);
+    // }
   }
 })
 
-//https://www.youtube.com/api/timedtext?sparams=asr_langs,caps,v,expire&expire=1423807305&asr_langs=en,pt,de,it,ru,ko,es,ja,nl,fr&hl=en_US&signature=E24D36B66C0F37BA26B699E50B3EC7BA41411260.7417BF773A8210565ACFC5F2D8321F4916B657E4&v=fPloDzu_wcI&key=yttt1&caps=asr
+//https://www.youtube.com/watch?v=fPloDzu_wcI
+//https://www.youtube.com/api/timedtext?sparams=asr_langs,caps,v,expire&expire=1423807305&asr_langs=en,pt,de,it,ru,ko,es,ja,nl,fr&hl=en_US&signature=E24D36B66C0F37BA26B699E50B3EC7BA41411260.7417BF773A8210565ACFC5F2D8321F4916B657E4&v=fPloDzu_wcI&key=yttt1&caps=asr&type=track&lang=en&name&kind=asr&fmt=1

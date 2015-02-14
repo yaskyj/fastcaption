9356c17c39568a52dd81ff63b9dbcdf947d7bba4
yaskyj <yaskyj@yahoo.com>
2015-02-14 11:55:17 -0600

    New captions.json file with new _id and video title

--- JSONConverter.js
+++ JSONConverter.js
@@ -1,7 +1,6 @@
 //This file is for converting all of the individual captions file into one JSON file for upload into mongodb  
 var fs = require('fs'),
     request = require('sync-request'),
-    sleep = require('sleep');
     reTitle = /<title>(.+) - YouTube</i,
     response,
     url,
@@ -9,11 +8,11 @@ var fs = require('fs'),
     filesArr,
     index,
     fileString,
+    title,
     filesArr = fs.readdirSync('.');
 
-
 for (index in filesArr) {
-  if (filesArr[index] != 'JSONConverter.js' && filesArr[index] != 'captions.json' && filesArr[index] != 'node_modules') {
+  if (filesArr[index] != 'JSONConverter.js' && filesArr[index] != 'captions.json' && filesArr[index] != 'node_modules' && filesArr[index] != '.DS_Store') {
     fileString = fs.readFileSync(filesArr[index]);
     url = 'https://www.youtube.com/watch?v=' + filesArr[index];
     console.log(url);
@@ -21,7 +20,8 @@ for (index in filesArr) {
     body = response.body.toString();
     titleStr = body.match(reTitle);
     console.log(response.statusCode + " and " + response.headers.toString());
-    console.log(titleStr[1]);
-    fs.appendFile('captions.json', '{"_id": "youtube' + filesArr[index] + '", "title": "' + titleStr[1] + '", "url": "https://www.youtube.com/watch?v=' + filesArr[index] + '", "captions" : ' + fileString + '}\n');
+    title = titleStr !== null ? titleStr[1] : 'need to add title manually'; 
+    console.log(title);
+    fs.appendFile('captions.json', '{"_id": "youtube' + filesArr[index] + '", "title": "' + title + '", "url": "https://www.youtube.com/watch?v=' + filesArr[index] + '", "captions" : ' + fileString + '}\n');
   }
 }
